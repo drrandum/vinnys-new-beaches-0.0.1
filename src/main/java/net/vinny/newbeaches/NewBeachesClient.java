d677939139aa06ba41c326539e7ab4c9dc7d88f8
vinny <vinnylupis@gmail.com>
2023-05-23 14:09:55 -0400

    commit

---
+++ src/main/java/net/vinny/newbeaches/NewBeachesClient.java
@@ -0,0 +1,10 @@
+package net.vinny.newbeaches;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class NewBeachesClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
