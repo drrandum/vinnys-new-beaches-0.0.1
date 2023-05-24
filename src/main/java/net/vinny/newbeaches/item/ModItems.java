3d65646731df35ea9f5dc7d819ea7d32a5aa7139
vinny <vinnylupis@gmail.com>
2023-05-24 18:02:06 -0400

    0.2.4

--- src/main/java/net/vinny/newbeaches/item/ModItems.java
+++ src/main/java/net/vinny/newbeaches/item/ModItems.java
@@ -2,10 +2,7 @@ package net.vinny.newbeaches.item;
 
 import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
 import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
-import net.minecraft.item.FoodComponent;
-import net.minecraft.item.Item;
-import net.minecraft.item.ItemGroup;
-import net.minecraft.item.ItemGroups;
+import net.minecraft.item.*;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
 import net.minecraft.util.Identifier;
