01096808d20fe81e70827dc83af5c9afa8a6f44c
vinny <vinnylupis@gmail.com>
2023-05-24 13:24:26 -0400

    0.1.2

--- src/main/java/net/vinny/newbeaches/item/ModItems.java
+++ src/main/java/net/vinny/newbeaches/item/ModItems.java
@@ -1,7 +1,10 @@
 package net.vinny.newbeaches.item;
 
 import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
+import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.item.ItemGroups;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
 import net.minecraft.util.Identifier;
@@ -11,13 +14,24 @@ public class ModItems {
 
     public static final Item COCONUT = registerItem("coconut",
             new Item(new FabricItemSettings()));
-
+    public static final Item COCONUT_FLESH = registerItem("coconut_flesh",
+            new Item(new FabricItemSettings()));
 
     private static Item registerItem(String name, Item item) {
         return Registry.register(Registries.ITEM, new Identifier(NewBeaches.MOD_ID, name), item);
     }
 
+    public static void addItemsToItemGroup() {
+        addToItemGroup(ItemGroups.FOOD_AND_DRINK, COCONUT);
+        addToItemGroup(ItemGroups.FOOD_AND_DRINK, COCONUT_FLESH);
+    }
+
+    private static void addToItemGroup(ItemGroup group, Item item) {
+        ItemGroupEvents.modifyEntriesEvent(group).register(entries -> entries.add(item));
+    }
+
     public static void registerModItems() {
         NewBeaches.LOGGER.info("Registering Mod Items for " + NewBeaches.MOD_ID);
+        addItemsToItemGroup();
     }
 }
