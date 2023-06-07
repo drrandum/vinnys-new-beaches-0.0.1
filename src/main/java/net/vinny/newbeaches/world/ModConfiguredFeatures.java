a680eda7c2fb27deec75141de97cae8d13b590df
vinny <vinnylupis@gmail.com>
2023-06-07 12:52:08 -0400

    laptop, dev art 4

--- src/main/java/net/vinny/newbeaches/world/ModConfiguredFeatures.java
+++ src/main/java/net/vinny/newbeaches/world/ModConfiguredFeatures.java
@@ -4,5 +4,9 @@ import net.minecraft.registry.RegistryKey;
 import net.minecraft.world.gen.feature.ConfiguredFeature;
 
 public class ModConfiguredFeatures {
-    //public static final RegistryKey<ConfiguredFeature<?, ?>> PALM_KEY =
+    /*public static final RegistryKey<ConfiguredFeature<?, ?>> PALM_KEY =
+
+    public static RegistryKey<ConfiguredFeature<?, ?>> registerKey(String name) {
+        return RegistryKey.of(RegistryKeys.)
+    }*/
 }
